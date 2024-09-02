453b25be597b4bb5811aad424e013e7e1e09a056
SwadhiSankar <swadhis14@gmail.com>
2024-09-02 14:49:31 +0530

    using switch, continue, break, return

--- bank.go
+++ bank.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	var accountBalance = 1000.0
+ var accountBalance = 1000.0
  fmt.Println("Go Bank")
+ for i:=0; i <4 ; i++ {
+   
+ 
+
  fmt.Println("What do you want to do ?")
  fmt.Println("1. Check balance")
  fmt.Println("2. Deposit Money")
@@ -15,38 +19,47 @@ var choice int
 fmt.Println("Enter choice : ")
 fmt.Scan(&choice)
 
-
-if choice == 1 {
-  fmt.Println("Balance :" ,accountBalance)
-  
-}else if choice ==2 {
+switch choice{
+case 1:
+	fmt.Println("Balance :" ,accountBalance)
+case 2:
 	fmt.Println("Your deposit : ")
 	var depositAmount float64
 	fmt.Scan(&depositAmount)
 	if depositAmount <=0{
 		fmt.Println("Invalid Amount. Should be greater than 0")
-		return
+		continue
 	}
 	accountBalance += depositAmount
 	fmt.Println("Total Bal after deposit:",accountBalance)
 
-}else if choice == 3{
+case 3:
 	fmt.Println("Your withdraw Amt : ")
 	var withdrawAmount float64
 	fmt.Scan(&withdrawAmount)
 
 	if withdrawAmount <=0{
 		fmt.Println("Invalid Amount. Must be greater than 0")
-		return
+		continue
 	}
 
 	if( withdrawAmount> accountBalance){
 		fmt.Println("Should not be greater than your account balance")
-		return
+		continue
 	}
 	accountBalance -= withdrawAmount
 	fmt.Println("Total Bal after withdraw:",accountBalance)
-}else{
-    fmt.Println("Wrong option! Check again")
+
+default:
+	fmt.Println("Wrong option! Check again")
+	fmt.Println("Thanks for choosing GO Bank")
+	return
 }
+
+
+
+
+}
+
+
 }
